137e0c763dda29a8ad328f4d9040ebec4bf415b1
Daniel <63912715+danielorkingit@users.noreply.github.com>
2022-08-01 19:56:19 +0200

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,14 +1,14 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-API_KEY = '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'
-HTMLMESSAGE= '<strong>WELCOME BACK TO MY HOUSE, DARLING</strong>'
+API_KEY = 'YOUR_API_KEY '
+HTMLMESSAGE= '<strong>YOUR MESSAGE WITHIN HTML</strong>'
 
 def mailer():
 
     message = Mail(
-    from_email='danielorkin06@gmail.com',
-    to_emails='orka3103@gmail.com',
+    from_email='SENDER_MAIL',
+    to_emails='RECIPIENT_EMAIL',
     subject='WELCOME',
     html_content= HTMLMESSAGE
     )
